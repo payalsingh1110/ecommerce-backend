0797d0565ada6f7d80b70b24cbf1b7b7302d313e
payalsingh1110 <payalsingh.111020@gmail.com>
2025-07-23 22:45:07 +0530

    WIR:work in progess for search and view all products by Name

---
+++ src/main/java/com/payal/ecom/services/customer/CustomerProductService.java
@@ -0,0 +1,12 @@
+package com.payal.ecom.services.customer;
+
+import com.payal.ecom.dto.ProductDto;
+
+import java.util.List;
+
+public interface CustomerProductService {
+
+    List<ProductDto> getAllProducts();
+
+    List<ProductDto> getAllProductsByName(String title);
+}
