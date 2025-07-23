b92f9e4bf3288df3cc6e41e8d5ffecea33bc0969
payalsingh1110 <payalsingh.111020@gmail.com>
2025-07-23 14:41:18 +0530

    Backend:  Product delete-by-ID working, image upload limit fix

--- src/main/java/com/payal/ecom/controller/admin/AdminProductController.java
+++ src/main/java/com/payal/ecom/controller/admin/AdminProductController.java
@@ -3,6 +3,7 @@ package com.payal.ecom.controller.admin;
 import com.payal.ecom.dto.ProductDto;
 import com.payal.ecom.services.admin.adminProduct.AdminProductService;
 import lombok.RequiredArgsConstructor;
+import org.hibernate.boot.jaxb.SourceType;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
@@ -23,7 +24,7 @@ public class AdminProductController {
     @PostMapping("/product")
     //instead of RequestBody bcz we want to accept the multipart file which is (img) in this product dto
     public ResponseEntity<ProductDto> addProduct(@ModelAttribute ProductDto productDto) throws IOException {
-
+        System.out.println("Product Details: " );
         ProductDto productDto1 = adminProductService.addProduct(productDto);
         return  ResponseEntity.status(HttpStatus.CREATED).body(productDto1);
 
@@ -37,4 +38,21 @@ public class AdminProductController {
         List<ProductDto> productDtos= adminProductService.getAllProducts();
         return ResponseEntity.ok(productDtos);
     }
+
+    @GetMapping("/search/{name}")
+    public ResponseEntity<List<ProductDto>> getAllProductByName(@PathVariable String name){
+
+        List<ProductDto> productDtos= adminProductService.getAllProductsByName(name);
+        return ResponseEntity.ok(productDtos);
+    }
+    @DeleteMapping("/product/{productId}")
+   public ResponseEntity<Void> deleteProduct(@PathVariable Long productId){
+        boolean deleted = adminProductService.deleteProduct(productId);
+        if(deleted){
+            return ResponseEntity.noContent().build();
+        }
+        return ResponseEntity.notFound().build();
+    }
+
+
 }
