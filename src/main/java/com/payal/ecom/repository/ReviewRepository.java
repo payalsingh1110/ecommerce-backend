c86858973d96dc5aaa641e97218d2fd07e4a8c0b
payalsingh1110 <payalsingh.111020@gmail.com>
2025-07-30 00:17:59 +0530

    Created REST API to fetch Product Details along with Reviews and FAQs using custom response DTO.

--- src/main/java/com/payal/ecom/repository/ReviewRepository.java
+++ src/main/java/com/payal/ecom/repository/ReviewRepository.java
@@ -1,10 +1,15 @@
 package com.payal.ecom.repository;
 
+import com.payal.ecom.entity.FAQ;
 import com.payal.ecom.entity.Review;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 
 @Repository
 public interface ReviewRepository extends JpaRepository<Review, Long> {
+
+    List<Review> findAllByProductId(Long productId);
 }
